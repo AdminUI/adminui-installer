4f3912377425086bc7c12b94150350c1b4c1ae7d
KennyTurner <kennyturner@ntlworld.com>
2024-10-01 14:03:19 +0000

    Run migrations in no-interaction mode #patch

--- src/Actions/RunMigrationsAction.php
+++ src/Actions/RunMigrationsAction.php
@@ -10,9 +10,12 @@ class RunMigrationsAction
     public function execute(bool $update = false)
     {
         $cmd = $update === true ? 'migrate' : 'migrate:fresh';
-        Artisan::call($cmd);
+        Artisan::call($cmd . " --no-interaction");
         $output = Artisan::output();
 
-        return Str::of($output)->explode("\n")->filter(fn ($item) => ! empty(trim($item)))->values();
+        return Str::of($output)
+            ->explode("\n")
+            ->filter(fn($item) => ! empty(trim($item)))
+            ->values();
     }
 }
